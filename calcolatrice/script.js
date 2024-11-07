f9314ed5397c5bc4a04fa3dae55f09fab5f7e61e
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2024-11-08 11:34:06 +1300

    js

--- calcolatrice/script.js
+++ calcolatrice/script.js
@@ -1,61 +1,79 @@
-	const un =["","",""];
-	let riss;
-	let i=0;
-	function scrivi(p1){
-	if(un[0]==''){
-	document.getElementById("testo").innerHTML  = ' ' ;
-	i=0;}
-	if(p1==('c')){
-	document.getElementById("testo").innerHTML  = ' ' ;
-	un[0]='';
-	un[1]='';
-	un[2]='';
-	i=0;
-	}else{
-    	document.getElementById("testo").innerHTML  += p1 ;
-	
-	if(p1 == '+' || p1 == '-' || p1 == '/' || p1 == '*'){
-	un[1]+=p1;
-	i+=2;
-	}else{
-	un[i]+=p1;}
+const un =["","","",""];
+let i=0;
+let riss;
+ function scrivi(p1){
+if(p1 == '+' || p1 == '-' || p1 == '/' || p1 == '*'){
+	if(un[0] == ''){
+		un[0]= un[3];
 	}
-
-    }
-    function operazione(){
-	riss = document.getElementById("testo").value ;
-    let ris;
-	switch(un[1]) {
-  		case '+':
-			if (un[0] == '')
-				ris=parseFloat(riss)+ parseFloat(un[2]);
-			else
-    			ris=parseFloat(un[0])+ parseFloat(un[2]);
-   			break;
-  		case '-':
-			if (un[0] == '')
-				ris=parseFloat(riss)- parseFloat(un[2]);
-			else
-    			ris=parseFloat(un[0])- parseFloat(un[2]);
-    		break;
-  		case '*':
-			if (un[0] == '')
-				ris=parseFloat(riss)* parseFloat(un[2]);
-			else
-    			ris=parseFloat(un[0])*parseFloat(un[2]);
-    		break;
-  		case '/':
-			if (un[0] == '')
-				ris=parseFloat(riss)/ parseFloat(un[2]);
-			else
-    			ris=parseFloat(un[0])/parseFloat(un[2]);
-    		break;
-  		default:
-    			ris='error';}
-	un[0]='';
+	document.getElementById("testo").innerHTML  = un[0];
 	un[1]='';
-	un[2]='';
-	document.getElementById("testo").innerHTML  = ris ;
-	return ris;
-    }
+	un[1]+=p1;
+	i=2;
+}
+if(un[0]==''){
+document.getElementById("testo").innerHTML  = ' ' ;
+i=0;}
+if(p1==('c')){
+document.getElementById("testo").innerHTML  = ' ' ;
+un[0]='';
+un[1]='';
+un[2]='';
+i=0;
+}else{
+		document.getElementById("testo").innerHTML  += p1 ;
+	un[i]+=p1;
+	if(p1 == '+' || p1 == '-' || p1 == '/' || p1 == '*'){
+
+		un.pop();
+		un[i]='';
+	}
+	console.log(un[0] + "  z");
+	console.log(un[1]) + "  u";
+	console.log(un[2]) + "  d";
+	if(un[0] == ""){
+		//un[0]=un[3];
+	}else{
+		switch(un[1]) {
+			  case '+':
+					un[3]=parseFloat(un[0])+ parseFloat(un[2]);
+				   break;
+			  case '-':
+				un[3]=parseFloat(un[0])- parseFloat(un[2]);
+					break;
+			  case '*':
+				un[3]=parseFloat(un[0])*parseFloat(un[2]);
+					break;
+			  case '/':
+				un[3]=parseFloat(un[0])/parseFloat(un[2]);
+					break;
+			}
+	}
+	console.log(un[3] + "  t");
+}
+}
+function operazione(){
+let ris;
+switch(un[1]) {
+	  case '+':
+			ris=parseFloat(un[0])+ parseFloat(un[2]);
+		   break;
+	  case '-':
+			ris=parseFloat(un[0])- parseFloat(un[2]);
+		break;
+	  case '*':
+			ris=parseFloat(un[0])*parseFloat(un[2]);
+		break;
+	  case '/':
+			ris=parseFloat(un[0])/parseFloat(un[2]);
+		break;
+	  default:
+			ris='error';}
+un[0]='';
+un[1]='';
+un[2]='';
+document.getElementById("testo").innerHTML  = ris ;
+}
+
+
 	
