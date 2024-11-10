99968a8ec64dfca43df49805ff0ee639386a77bb
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2024-11-10 21:14:04 +0100

    progressi

--- matrice/script.js
+++ matrice/script.js
@@ -1,28 +1,23 @@
 let a;
-console.log(document.getElementById("numeropari").value)
-if(document.getElementById("numeropari").value > 2 && document.getElementById("numeropari").value % 2 == 0){
-   a = document.getElementById("numeropari").value;
-}else{
-    a=4;
-}
-console.log(a)
-const matrice = new Array(a);
-for(let i=0;i<matrice.length;i++){
-    matrice[i] = new Array(matrice.length);
-    for(let j=0;j<matrice.length;j++){
-        let array = matrice[i];
-        array[j] = parseInt( Math.random()*100);
-        
+let matrice;
+function creaTabella(){
+    if(document.getElementById("numeropari").value > 2 && document.getElementById("numeropari").value % 2 == 0){
+        a = document.getElementById("numeropari").value;
+    }else{
+        a=4;
     }
-}
-console.log( matrice)
-
-
-const table = document.getElementById("demo");
-matrice.forEach(element =>{
+    matrice = new Array(a);
+    for(let i=0;i<a;i++){
+        matrice[i] = new Array(matrice.length);
+        for(let j=0;j<a;j++){
+            let array = matrice[i];
+            array[j] = parseInt( Math.random()*100);
+        }
+    }
+    const table = document.getElementById("demo");
+    matrice.forEach(element =>{
     const riga = document.createElement("tr");
     Object.keys(element).forEach(elemento => {
-console.log(element[elemento])
         const colonna = document.createElement("td");
         const testo = document.createTextNode(element[elemento]);
         colonna.appendChild(testo);
@@ -30,3 +25,15 @@ console.log(element[elemento])
     })
     table.appendChild(riga);
 })
+}
+function giraDestra(num){
+    for(let i=1;i<num;i++){
+        for(let j=0;j<num;j++){
+
+        }
+    }
+
+}
+function giraSinistra(){
+    
+}
