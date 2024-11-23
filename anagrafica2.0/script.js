9bbea11d0ea6eef910360c61f10c7d87e7ebad1f
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2024-11-23 15:12:12 +0100

    2

--- anagrafica2.0/script.js
+++ anagrafica2.0/script.js
@@ -1,13 +1,15 @@
-const matrice=[];
+const array=[];
 function bottone()
 {
-matrice[0]= document.getElementById("nome").value;
-matrice[1]= document.getElementById("cognome").value;
-matrice[2]= document.getElementById("indirizzo").value;
-matrice[3]= document.getElementById("città").value;
-document.getElementById("nome").value="";
-document.getElementById("cognome").value="";
-document.getElementById("città").value="";
-document.getElementById("indirizzo").value="";
-document.getElementById("demo").innerHTML += "<tr><td>" + matrice[0] + "</td><td>" + matrice[1] + "</td><td>" + matrice[2] + "</td><td>" + matrice[3] + "</td></tr>";
+    array[0]= document.getElementById("nome").value;
+    array[1]= document.getElementById("cognome").value;
+    array[2]= document.getElementById("indirizzo").value;
+    array[3]= document.getElementById("città").value;
+    document.getElementById("nome").value="";
+    document.getElementById("cognome").value="";
+    document.getElementById("città").value="";
+    document.getElementById("indirizzo").value="";
+    if(array[0]!==undefined||array[1]!==undefined||array[2]!==undefined||array[3]!==undefined){
+        document.getElementById("demo").innerHTML += "<tr><td>" + array[0] + "</td><td>" + array[1] + "</td><td>" + array[2] + "</td><td>" + array[3] + "</td></tr>";
+    }
 }
