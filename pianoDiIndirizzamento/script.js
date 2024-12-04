51324b33669c5d151f22c3ff978f8fba56517388
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2024-12-04 21:41:55 +0100

    indirizzamento

--- pianoDiIndirizzamento/script.js
+++ pianoDiIndirizzamento/script.js
@@ -1,8 +1,7 @@
 function genera(){
     const pl = document.getElementById("prefixLength").value;
     const IP = document.getElementById("IP-Address").value;
-    const bin = document.getElementById("tabella");
-    const dec = document.getElementById("dec");
+    const tabella = document.getElementById("tabella");
     let ip = IP.split(".");
     let ipbin =[parseInt(ip[0]).toString(2),parseInt(ip[1]).toString(2),parseInt(ip[2]).toString(2),parseInt(ip[3]).toString(2)];
     for (let i =0;i<ipbin.length;i++){
@@ -21,9 +20,7 @@ function genera(){
     let broadcast = broadcast_dec.toString(2);
     for ( let i=1;i < broadcast_dec;i++){
         dhost=i;
-        bin.innerHTML += "<tr><td id='net'>" + net + "</td><td id='host'>" + dhost.toString(2) + "</td><td>" + pl + "</td></tr>";
-        dec.innerHTML += "<tr><td id='net'>" + dnet + "</td><td id='host'>" + dhost + "</td><td>" + pl + "</td></tr>";
+        tabella.innerHTML += "<tr><td id='net'>" + net + "</td><td id='host'>" + dhost.toString(2) + "</td><td id='net'>" + dnet + "</td><td id='host'>" + dhost + "</td><td>" + pl + "</td></tr>";
     }
-    bin.innerHTML += "<tr><td id='net'>" + net + "</td><td id='host'>" + broadcast + "</td><td>" + pl + "</td></tr>";
-    dec.innerHTML += "<tr><td id='net'>" + dnet + "</td><td id='host'>" + broadcast_dec + "</td><td>" + pl + "</td></tr>";
+    tabella.innerHTML += "<tr><td id='net'>" + net + "</td><td id='host'>" + broadcast + "</td><td id='net'>" + dnet + "</td><td id='host'>" + broadcast_dec + "</td><td>" + pl + "</td></tr>";
 }
