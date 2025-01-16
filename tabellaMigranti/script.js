6d5ea9600c838b8d5451f9d19d5bc81cbf732922
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-01-16 11:54:06 +0100

    mess

--- tabellaMigranti/script.js
+++ tabellaMigranti/script.js
@@ -7,3 +7,26 @@ document.getElementById("anno").value="";
 document.getElementById("numero").value="";
 document.getElementById("demo").innerHTML += "<tr><td>" + matrice[0] + "</td><td>" + matrice[1] + "</td></tr>";
 }
+function leggiFile(input){
+
+    let file = input.files[0] //presa in input del file
+    let lettoreF = new FileReader() //creazione oggetto filereader
+    lettoreF.readAsText(file);
+    
+    lettoreF.onload = function() {
+    let contenuto = lettoreF.result; // Contenuto del file letto
+    creaTab(contenuto);
+    }
+}
+function creaTab(tab){
+    let righe= tab.split("\n")
+    for (int i = 0; i < righe.length; i++) {
+        const colonne = righe[i].split(',')
+    }
+    for (int i = 0; i < colonne.length; i++) {
+        const colonne1 = colonne[i].slice(1,-1)
+    }
+    for (int i = 0; i < colonne.length; i++) {
+        ocument.getElementById("demo").innerHTML += "<tr><td>" + colonne[0] + "</td><td>" + matrice[1] + "</td></tr>";
+    }
+}
