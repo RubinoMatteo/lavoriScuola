d49030dc44bd3562d9b4ceef25d5b4e14dd741b4
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-01-21 23:31:45 +0100

    LLAv1.2(debag)

--- creaLLA/script.js
+++ creaLLA/script.js
@@ -1,20 +1,22 @@
 function generaLLA(){
-    let MAC = document.getElementById("MAC-Address").value;;
-    let IPv6 = document.getElementById("IPv6").value;;
+    let MAC = document.getElementById("MAC-Address").value;
+    let IPv6 = document.getElementById("IPv6").value;
     if(MAC.length < 12||MAC.length > 17){
         alert("MAC address non valido ");
-        MAC = "";
+        document.getElementById("MAC-Address").value = "";
     }
     let i;
     let prima_parte;
     let seconda_parte;
     let Host;
     if (MAC.length == 17){
-        let r = MAC.spilt(".");
+        let R= MAC[2];
+        let r = MAC.spilt(R);
         i = r.join("");
         prima_parte=i.slice(0,6)
-        seconda_parte=i.slice(6,16)
+        seconda_parte=i.slice(6,12)
         Host =prima_parte+"fffe"+seconda_parte;
+        console.log(Host)
         cambia = parseInt(r.slice(0,2).join(""),16);
         cambia = cambia.toString(2);
         if (cambia[6]===0){
@@ -29,8 +31,9 @@ function generaLLA(){
         IPv6="fe80::"+host+" /64";
     }else{
         prima_parte=MAC.slice(0,6)
-        seconda_parte=MAC.slice(6,16)
+        seconda_parte=MAC.slice(6,12)
         Host =prima_parte+"fffe"+seconda_parte;
+        console.log(Host)
         cambia = parseInt(MAC.slice(0,2),16);
         cambia = cambia.toString(2);
         if (cambia[6]===0){
