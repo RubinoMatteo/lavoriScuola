d58b3d58d4e0d3f42d1756efcf70f41b8ca620a3
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-01-21 22:01:10 +0100

    MigrantiV2.0

--- tabellaMigranti/script.js
+++ tabellaMigranti/script.js
@@ -23,8 +23,10 @@ function creaTab(tab){
     let righe= tab.split("\n")
     let colonne=[]
     let colonne1=[]
-    for (let i = 0; i < righe.length; i++) {
-        colonne[i] = righe[i].split(',')
+    for (let i = 0; i < righe.length*2; i+2) {
+        for (let j = 0; j < righe.length; j++) {
+        colonne[i] = righe[j].split(',')[0]
+        colonne[i+1] = righe[j].split(',')[1]}
     }
     for (let i = 0; i < colonne.length; i++) {
         colonne1[i] = colonne[i].slice(1,-1)
@@ -34,6 +36,16 @@ function creaTab(tab){
             document.getElementById("demo").innerHTML += "<tr><td>" + colonne1[i] + "</td><td>" + colonne1[i+1] + "</td></tr>";
         }
     }
+    /*for (let i = 0; i < righe.length; i++) {
+        colonne[i] = righe[i].split(',')
+    }
+    for (let i = 0; i < colonne.length; i++) {
+        for (let j = 0; j < colonne[i].length; j++) {
+        colonne1[i][j] = colonne[i][j].slice(1,-1)
+    }
+    for (let i = 0; i < colonne.length; i++) {
+            document.getElementById("demo").innerHTML += "<tr><td>" + colonne1[i][0] + "</td><td>" + colonne1[i][1] + "</td></tr>";
+    }*/
     console.log(colonne1)
     console.log(colonne)
     console.log(righe)
