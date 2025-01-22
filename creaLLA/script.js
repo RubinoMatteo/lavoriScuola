9f4000cb4506457b9c24ee7e3eb24294cfb17581
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-01-22 23:30:33 +0100

    LLAv3.0

--- creaLLA/script.js
+++ creaLLA/script.js
@@ -3,50 +3,83 @@ function generaLLA(){
     let IPv6 = document.getElementById("IPv6").value;
     if(MAC.length < 12||MAC.length > 17){
         alert("MAC address non valido ");
-        document.getElementById("MAC-Address").value = "";
+        MAC = "";
     }
     let i;
     let prima_parte;
     let seconda_parte;
     let Host;
     if (MAC.length == 17){
-        let R= MAC[2];
-        let r = MAC.spilt(R);
+	let s= MAC[2]
+        let r = MAC.split(s);
         i = r.join("");
         prima_parte=i.slice(0,6)
-        seconda_parte=i.slice(6,12)
+        seconda_parte=i.slice(6,16)
         Host =prima_parte+"fffe"+seconda_parte;
-        console.log(Host)
-        cambia = parseInt(r.slice(0,2).join(""),16);
+        cambia = parseInt(i.slice(0,2),16);
         cambia = cambia.toString(2);
-        if (cambia[6]===0){
-            cambia[6]="1";
+	if (cambia.length<8){
+            let lun= cambia.length;
+            for (let j = 0;j<(8-lun);j++){
+                cambia="0"+cambia;
+            }
+        }
+        if (cambia.at(6)==0){
+            let c =cambia.slice(6,8);
+		c='1'.concat('',cambia.at(7));
+		let d =cambia.slice(0,6);
+		cambia=d.concat("",c)
         }else{
-            cambia[6]="0";
+		let c =cambia.slice(6,8);
+		c='0'.concat('',cambia[7]);
+		let d=cambia.slice(0,5)
+		cambia=d.concat("",c)
         }
         cambia = parseInt(cambia,2);
         cambia = cambia.toString(16);
-        [Host[0],Host[1]]=cambia;
-        host = Host.slice(0,4)+":"+ Host.slice(4,8)+":"+ Host.slice(8,12)+":"+ Host.slice(12,16);
-        IPv6="fe80::"+host+" /64";
+	if (cambia.length<2){
+            let lun= cambia.length;
+            for (let j = 0;j<(2-lun);j++){
+                cambia="0"+cambia;
+            }
+        }
+        Host=cambia.concat(Host.slice(2,16));
+        let host = Host.slice(0,4)+":"+ Host.slice(4,8)+":"+ Host.slice(8,12)+":"+ Host.slice(12,16);
+	document.getElementById("IPv6").value="fe80::"+host+" /64";
     }else{
         prima_parte=MAC.slice(0,6)
-        seconda_parte=MAC.slice(6,12)
+        seconda_parte=MAC.slice(6,16)
         Host =prima_parte+"fffe"+seconda_parte;
-        console.log(Host)
         cambia = parseInt(MAC.slice(0,2),16);
         cambia = cambia.toString(2);
-        if (cambia[6]===0){
-            cambia[6]="1";
+	if (cambia.length<8){
+            let lun= cambia.length;
+            for (let j = 0;j<(8-lun);j++){
+                cambia="0"+cambia;
+            }
+        }
+	if (cambia.at(6)==0){
+            let c =cambia.slice(6,8);
+		c='1'.concat('',cambia.at(7));
+		let d =cambia.slice(0,6);
+		cambia=d.concat("",c)
         }else{
-            cambia[6]="0";
+		let c =cambia.slice(6,8);
+		c='0'.concat('',cambia[7]);
+		let d=cambia.slice(0,5)
+		cambia=d.concat("",c)
         }
         cambia = parseInt(cambia,2);
         cambia = cambia.toString(16);
-        [Host[0],Host[1]]=cambia;
+	if (cambia.length<2){
+            let lun= cambia.length;
+            for (let j = 0;j<(2-lun);j++){
+                cambia="0"+cambia;
+            }
+        }
+        Host=cambia.concat(Host.slice(2,16));
         host = Host.slice(0,4)+":"+ Host.slice(4,8)+":"+ Host.slice(8,12)+":"+ Host.slice(12,16);
-        IPv6="fe80::"+host+" /64";
+        document.getElementById("IPv6").value="fe80::"+host+" /64";
     }
-
 
 }
