577fc5cdd93d66ffc77a4ae42dff590bc522e180
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-01-23 18:33:37 +0100

    FIFOv2.0

--- codaFIFO/script.js
+++ codaFIFO/script.js
@@ -1,15 +1,17 @@
-const coda={};
-function inserisci()
-{
+const coda=[];
+function inserisci(){
 coda.unshift( {nome: document.getElementById("nome").value ,
     cognome: document.getElementById("cognome").value});
 document.getElementById("nome").value="";
 document.getElementById("cognome").value="";
 }
-function preleva()
-{
-    let coda2=coda.reverse();
-    let uscito =coda2.shift();
-    document.getElementById("text").innerHTML  = uscito.nome +"," +uscito.cognome;
-    coda.pop();
+function preleva(){
+    if (coda.length>0){
+        let coda2=coda.reverse();
+        let uscito =coda2.shift();
+        document.getElementById("text").innerHTML  = uscito.nome +"," +uscito.cognome;
+        coda2=coda2.reverse();
+    }else{
+        document.getElementById("testo").value  = 'CODA VUOTA' ;
+    }
 }
