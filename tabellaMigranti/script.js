4083f9d514a88357f3abf1411b3accb3e02a7c1c
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-01-23 17:25:30 +0100

    tabella migrantiV3.0

--- tabellaMigranti/script.js
+++ tabellaMigranti/script.js
@@ -1,12 +1,3 @@
-/*const matrice=[];
-function bottone()
-{
-matrice[0]= document.getElementById("anno").value;
-matrice[1]= document.getElementById("numero").value;
-document.getElementById("anno").value="";
-document.getElementById("numero").value="";
-document.getElementById("demo").innerHTML += "<tr><td>" + matrice[0] + "</td><td>" + matrice[1] + "</td></tr>";
-}*/
 function leggiFile(input){
 
     let file = input.files[0] //presa in input del file
@@ -16,37 +7,23 @@ function leggiFile(input){
     lettoreF.onload = function() {
     let contenuto = lettoreF.result; // Contenuto del file letto
     creaTab(contenuto);
-    console.log(contenuto)
     }
 }
 function creaTab(tab){
     let righe= tab.split("\n")
     let colonne=[]
     let colonne1=[]
-    /*for (let i = 0; i < righe.length*2; i+2) {
-        for (let j = 0; j < righe.length; j++) {
-        colonne[i] = righe[j].split(',')[0]
-        colonne[i+1] = righe[j].split(',')[1]}
-    }
-    for (let i = 0; i < colonne.length; i++) {
-        colonne1[i] = colonne[i].slice(1,-1)
-    }
-    for (let i = 0; i < colonne.length; i++) {
-        if(i%2==0){
-            document.getElementById("demo").innerHTML += "<tr><td>" + colonne1[i] + "</td><td>" + colonne1[i+1] + "</td></tr>";
-        }
-    }*/
     for (let i = 0; i < righe.length; i++) {
         colonne[i] = righe[i].split(',')
     }
     for (let i = 0; i < colonne.length; i++) {
         for (let j = 0; j < colonne[i].length; j++) {
-        colonne1[i][j] = colonne[i][j].slice(1,-1)
+        	colonne1[2*i+j] = colonne[i][j].replaceAll('"','')
+    	}
     }
-    for (let i = 0; i < colonne.length; i++) {
-            document.getElementById("demo").innerHTML += "<tr><td>" + colonne1[i][0] + "</td><td>" + colonne1[i][1] + "</td></tr>";
+    for (let i = 0; i < colonne1.length; i++) {
+	if (i%2==0){
+            document.getElementById("demo").innerHTML += "<tr><td>" + colonne1[i] + "</td><td>" + colonne1[i+1] + "</td></tr>";
+	}
     }
-    console.log(colonne1)
-    console.log(colonne)
-    console.log(righe)
 }
