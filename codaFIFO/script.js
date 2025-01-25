7642665815284d7685350120c8f5f86b54048adf
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-01-25 16:47:11 +0100

    fileOriginale

--- codaFIFO/script.js
+++ codaFIFO/script.js
@@ -1,17 +1,18 @@
 const coda=[];
 function inserisci(){
-coda.unshift( {nome: document.getElementById("nome").value ,
-    cognome: document.getElementById("cognome").value});
+coda.unshift({nome: document.getElementById("nome").value ,
+		cognome: document.getElementById("cognome").value});
 document.getElementById("nome").value="";
 document.getElementById("cognome").value="";
 }
 function preleva(){
-    if (coda.length>0){
-        let coda2=coda.reverse();
-        let uscito =coda2.shift();
-        document.getElementById("text").innerHTML  = uscito.nome +"," +uscito.cognome;
-        coda2=coda2.reverse();
-    }else{
-        document.getElementById("testo").value  = 'CODA VUOTA' ;
-    }
+if (coda.length>0){
+
+    let coda2=coda.reverse();
+    let uscito =coda2.shift();
+    document.getElementById("testo").value  = uscito.nome+','+uscito.cognome ;
+coda2=coda2.reverse();
+}else{
+	document.getElementById("testo").value  = 'CODA VUOTA' ;
+}
 }
