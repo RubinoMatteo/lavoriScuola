0bd7ed28ca24291613719ea177eaba59fed2edcb
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-03-12 21:09:21 +0100

    cV5

--- collection/script.js
+++ collection/script.js
@@ -15,4 +15,24 @@ function scrivi(){
     document.getElementById("indirizzo").value="";
     const data = JSON.stringify(array);
     localStorage.setItem("data", myJSON);
+}function tabella(){
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/collection/index2.html";
+}
+const array = [];
+function scrivi(){
+    const userData = {
+        nome: document.getElementById("nome").value,
+        cognome: document.getElementById("cognome").value,
+        indirizzo: document.getElementById("indirizzo").value,
+        età: document.getElementById("età").value,
+        sesso: document.getElementById("sesso").value
+    };
+    array.push(userData);
+    document.getElementById("nome").value = "";
+    document.getElementById("cognome").value = "";
+    document.getElementById("indirizzo").value = "";
+    document.getElementById("età").value = "";
+            document.getElementById("sesso").value = "";
+    const data = JSON.stringify(array);
+            localStorage.setItem("data", data);
 }
