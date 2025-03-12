a03c86ce221d95f827f57a33f68bf3995dcfdb1d
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-03-12 21:59:50 +0100

    cv5.4

--- collection/script2.js
+++ collection/script2.js
@@ -11,11 +11,9 @@ function generaTabella(arrObj){
         const riga = document.createElement("tr");
         Object.keys(element).forEach(elemento => {
             const colonna = document.createElement("td");
-            colonna.id= "e"+index;
             const testo = document.createTextNode(element[elemento]);
             colonna.appendChild(testo);
             riga.appendChild(colonna);
-            index++;
         })
         table.appendChild(riga);
     })
