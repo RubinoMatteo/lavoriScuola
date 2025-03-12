1e878258be105b5f4a855805f2df2b3ce34b87d2
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-03-12 19:31:55 +0100

    cV2.3

--- collection/script.js
+++ collection/script.js
@@ -5,10 +5,10 @@ function vai_alla_tabella(){
 const array=[];
 function bottone(){
     array+={ nome:document.getElementById("nome").value
-        cognome: document.getElementById("cognome").value
-        indirizzo: document.getElementById("indirizzo").value
-        età: document.getElementById("età").value
-        seeso: document.getElementById("sesso").value};
+        cognome:document.getElementById("cognome").value
+        indirizzo:document.getElementById("indirizzo").value
+        età:document.getElementById("età").value
+        seeso:document.getElementById("sesso").value};
     document.getElementById("nome").value="";
     document.getElementById("cognome").value="";
     document.getElementById("città").value="";
