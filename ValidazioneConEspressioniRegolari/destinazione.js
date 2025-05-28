d13819b634ef1ccdde66f00e54ebe70ac2c7b5a0
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-05-28 20:26:58 +0200

    Validazione V1.0

---
+++ ValidazioneConEspressioniRegolari/destinazione.js
@@ -0,0 +1,10 @@
+document.getElementById('nome').innerText = localStorage.getItem('nom');
+document.getElementById('cognome').innerText = localStorage.getItem('cognom');
+document.getElementById('comune').innerText = localStorage.getItem('com');
+document.getElementById('cap').innerText = localStorage.getItem('c');
+document.getElementById('telefono').innerText = localStorage.getItem('tel');
+document.getElementById('email').innerText = localStorage.getItem('em');
+
+
+document.getElementById('password').innerText = localStorage.getItem('pwd');
+document.getElementById('conferma').innerText = localStorage.getItem('con');
