66f79378b00f4069e1783c0ee92a01c697419d27
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-10-30 16:32:54 +0100

    correzzioni errori di battitura

--- anno2025-26/passaggioDati/v3/salva_dati.php
+++ anno2025-26/passaggioDati/v3/salva_dati.php
@@ -29,7 +29,7 @@ fclose($file_w);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dati Salvati con Successo</title>
-    <link rel="icon" href="https://rubinomatteo.github.io/lavoriScuola/imagini/rubi.jpg" type="image/jpg" />
+    <link rel="icon" href="https://rubinomatteo.github.io/lavoriScuola/immagini/rubi.jpg" type="image/jpg" />
 
     <style>
         * {
