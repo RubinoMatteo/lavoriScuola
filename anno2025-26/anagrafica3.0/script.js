a2aecdace7cd227fb07f4f69d7370881b41be859
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-11-20 23:23:43 +0100

    correrezione errore battitura

--- anno2025-26/anagrafica3.0/script.js
+++ anno2025-26/anagrafica3.0/script.js
@@ -7,7 +7,7 @@ function apple(){
     localStorage.setItem(0, "apple");
     location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
-function hauawei(){
+function huawei(){
     localStorage.setItem(0, "hauawei");
     location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
