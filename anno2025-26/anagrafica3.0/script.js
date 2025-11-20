9be2f205142a343cb7d725a71c42e4dda1b4c175
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-11-20 23:14:46 +0100

    modifica link

--- anno2025-26/anagrafica3.0/script.js
+++ anno2025-26/anagrafica3.0/script.js
@@ -1,13 +1,13 @@
 
 function samsung(){
     localStorage.setItem(0, "samsung");
-    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/pagina2/index.html";
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
 function apple(){
     localStorage.setItem(0, "apple");
-    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/pagina2/index.html";
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
 function hauawei(){
     localStorage.setItem(0, "hauawei");
-    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/pagina2/index.html";
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
