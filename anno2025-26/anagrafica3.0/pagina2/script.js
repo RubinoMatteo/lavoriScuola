14bb0e218115788c7f5295c5f6257971d67eb27c
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-11-22 16:28:06 +0100

    download

--- anno2025-26/anagrafica3.0/pagina2/script.js
+++ anno2025-26/anagrafica3.0/pagina2/script.js
@@ -25,7 +25,7 @@ xmlhttp.open("GET", "samsung.json", true);
                             <p><b>Memoria</b>:${myObj.samsung[x].memory}</p>
                             <p><b>S.O.</b>:${myObj.samsung[x].os}</p>
                             <img id="img" src="${myObj.samsung[x].image}" alt="samsung"> 
-                            <a target="_blank" class="button" onclick="acquista('${myObj.samsung[x].name}')" > &#128722; </a>
+                            <a target="_blank" class="button" onclick="acquista('${myObj.samsung[x].name}','${myObj.samsung[x].memory}','${myObj.samsung[x].os}','${myObj.samsung[x].image}')" > &#128722; </a>
                         </div>`;
             document.getElementById("demo").innerHTML = `${stampa}`;
             stampa="";
@@ -52,7 +52,7 @@ function apple(){
                             <p><b>Memoria</b>:${memory}</p>
                             <p><b>S.O.</b>:${os}</p>
                             <img id="img" src="${image}" alt="iPhone"> 
-                            <a target="_blank" class="button" onclick="acquista('${name}')" > &#x1f6d2; </a>
+                            <a target="_blank" class="button" onclick="acquista('${name}','${memory}','${os}','${image}')" > &#x1f6d2; </a>
                         </div> `;
             }
             
@@ -88,7 +88,7 @@ function huawei(){
                             <p><b>Memoria</b>:${huawei[i][1]}</p>
                             <p><b>S.O.</b>:${huawei[i][2]}</p>
                             <img id="img" src="${huawei[i][3]}" alt="iPhone"> 
-                            <a target="_blank" class="button" onclick="acquista('${huawei[i][0]}')" > &#x1f6d2; </a>
+                            <a target="_blank" class="button" onclick="acquista('${huawei[i][0]}','${huawei[i][1]}','${huawei[i][2]}','${huawei[i][3]}')" > &#x1f6d2; </a>
                             </div> `;
             }
             document.getElementById("demo").innerHTML = `${stampa}`;
@@ -105,8 +105,39 @@ function errore(){
 };
 var carrello=[];
 let quantità=0;
-function acquista(obj){
-    carrello[quantità]= obj;
+function acquista(n,m,S,i){
+    carrello=JSON.parse(localStorage.getItem(1));
+    quantità=JSON.parse(localStorage.getItem(1)).length;
+    carrello[quantità]= {name:n,memory:m,"OS":OS,"img":img};
     quantità++;
+    localStorage.setItem(1, JSON.stringify(carrello));
     console.log(carrello);
 };
+
+const linkScarica = document.getElementById('linkScaricaJson');
+linkScarica.addEventListener('click', function(event) {
+    
+    // Converti l'oggetto JavaScript in una stringa JSON formattata
+    const stringaJson = localStorage.getItem(1); 
+    
+    // Crea un Blob con il contenuto JSON e specifica il tipo MIME corretto
+    const blob = new Blob([stringaJson], { type: 'application/json' });
+    
+    // Genera un URL temporaneo che punta al Blob
+    const url = URL.createObjectURL(blob);
+    
+    // Imposta l'attributo 'href' del link con l'URL temporaneo
+    // L'attributo 'download' (che hai già nell'HTML) gestirà il nome del file
+    linkScarica.href = url;
+    
+    // Nota importante: il browser gestirà automaticamente il download a questo punto
+    // grazie al fatto che abbiamo impostato href e download prima che l'azione di default del click avvenga.
+
+    // Opzionale, ma consigliato: rilascia l'URL dell'oggetto dopo un breve ritardo
+    // per liberare memoria. Visto che il browser inizia subito il download, 
+    // un timeout breve è sicuro.
+    setTimeout(() => {
+        URL.revokeObjectURL(url);
+    }, 1000); // Rilascia l'URL dopo 1 secondo
+    localStorage.clear();
+});
