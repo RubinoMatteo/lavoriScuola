1d6be3799dedae63c0b4a751adbe593aefdcc644
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-11-22 22:58:57 +0100

    sessionStorage

--- anno2025-26/anagrafica3.0/script.js
+++ anno2025-26/anagrafica3.0/script.js
@@ -1,15 +1,15 @@
 array=[];
-localStorage.getItem(1) !== null? array=JSON.parse(localStorage.getItem(1)) : array[0]={name:"nome",memory:"memoria",OS:"Sistema Operativo"};
-localStorage.setItem(1, JSON.stringify(array, null, 2));
+sessionStorage.getItem(1) !== null? array=JSON.parse(localStorage.getItem(1)) : array[0]={name:"nome",memory:"memoria",OS:"Sistema Operativo"};
+sessionStorage.setItem(1, JSON.stringify(array, null, 2));
 function samsung(){
-    localStorage.setItem(0, "samsung");
+    sessionStorage.setItem(0, "samsung");
     location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
 function apple(){
-    localStorage.setItem(0, "apple");
+    sessionStorage.setItem(0, "apple");
     location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
 function huawei(){
-    localStorage.setItem(0, "huawei");
+    sessionStorage.setItem(0, "huawei");
     location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
