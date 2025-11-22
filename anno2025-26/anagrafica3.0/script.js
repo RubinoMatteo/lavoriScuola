fc228ecfa651ea4224ab7aa8407fe53b864f3d94
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-11-22 18:29:00 +0100

    download 1.7

--- anno2025-26/anagrafica3.0/script.js
+++ anno2025-26/anagrafica3.0/script.js
@@ -1,5 +1,5 @@
 array=[];
-array[0]= localStorage.getItem(1) !== null? JSON.parse(localStorage.getItem(1)) : {name:"nome",memory:"memoria",OS:"Sistema Operativo"};
+localStorage.getItem(1) !== null? array=JSON.parse(localStorage.getItem(1)) : array[0]={name:"nome",memory:"memoria",OS:"Sistema Operativo"};
 localStorage.setItem(1, JSON.stringify(array));
 function samsung(){
     localStorage.setItem(0, "samsung");
