e1ed3bad3eeb055bc3c507b0025fe87bea7e8ed2
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-11-22 14:29:43 +0100

    sistemo leggibilità

--- anno2025-26/anagrafica3.0/pagina2/script.js
+++ anno2025-26/anagrafica3.0/pagina2/script.js
@@ -20,7 +20,13 @@ xmlhttp.open("GET", "samsung.json", true);
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             for (x in myObj.samsung) 
-                stampa += `<div class="card"><h3>${myObj.samsung[x].name}</h3><p>${myObj.samsung[x].memory}</p><p>${myObj.samsung[x].os}</p><img id="img" src="${myObj.samsung[x].image}" alt="samsung"> <a target="_blank" class="button" onclick="acquista('${myObj.samsung[x].name}')" > &#128722; </a></div>`;
+                stampa += `<div class="card">
+                            <h3>${myObj.samsung[x].name}</h3>
+                            <p><b>Memoria</b>:${myObj.samsung[x].memory}</p>
+                            <p><b>S.O.</b>:${myObj.samsung[x].os}</p>
+                            <img id="img" src="${myObj.samsung[x].image}" alt="samsung"> 
+                            <a target="_blank" class="button" onclick="acquista('${myObj.samsung[x].name}')" > &#128722; </a>
+                        </div>`;
             document.getElementById("demo").innerHTML = `${stampa}`;
             stampa="";
         }
@@ -41,7 +47,13 @@ function apple(){
                 var memory = iPhone[x].getElementsByTagName("memory")[0].childNodes[0].nodeValue;
                 var os = iPhone[x].getElementsByTagName("os")[0].childNodes[0].nodeValue;
                 var image = iPhone[x].getElementsByTagName("image")[0].childNodes[0].nodeValue;
-                stampa+=`<div class="card"><h3>${name}</h3><p>${memory}</p><p>${os}</p><img id="img" src="${image}" alt="iPhone"> <a target="_blank" class="button" onclick="acquista('${name}')" > &#x1f6d2; </a></div> `;
+                stampa+=`<div class="card">
+                            <h3>${name}</h3>
+                            <p><b>Memoria</b>:${memory}</p>
+                            <p><b>S.O.</b>:${os}</p>
+                            <img id="img" src="${image}" alt="iPhone"> 
+                            <a target="_blank" class="button" onclick="acquista('${name}')" > &#x1f6d2; </a>
+                        </div> `;
             }
             
             document.getElementById("demo").innerHTML = `${stampa}`;
@@ -73,7 +85,7 @@ function huawei(){
             for (let i = 1; i < huawei.length; i++) {
                 stampa+=`<div class="card">
                             <h3>${huawei[i][0]}</h3>
-                            <p>${huawei[i][1]}</p>
+                            <p><b>Memoria</b>:${huawei[i][1]}</p>
                             <p><b>S.O.</b>:${huawei[i][2]}</p>
                             <img id="img" src="${huawei[i][3]}" alt="iPhone"> 
                             <a target="_blank" class="button" onclick="acquista('${huawei[i][0]}')" > &#x1f6d2; </a>
