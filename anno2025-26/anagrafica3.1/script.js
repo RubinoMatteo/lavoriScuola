80ec31440e8b02de4ce7d806c769e569050f0bd8
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-12-07 11:31:59 +0100

    scontrino 1.8

--- anno2025-26/anagrafica3.1/script.js
+++ anno2025-26/anagrafica3.1/script.js
@@ -252,13 +252,19 @@ function scaricaPDF(event) {
     // Costruzione del contenuto testuale per il PDF
     let righe = [];
     righe.push("CARRELLO ACQUISTI");
-    righe.push("");
-    righe.push(colonne.join(" | "));
+    //righe.push("");
+    //righe.push(colonne.join(" | "));
     righe.push("-".repeat(50));
 
     dati.forEach(obj => {
-        const valori = colonne.map(col => String(obj[col]));
-        righe.push(valori.join(" | "));
+        //const valori = colonne.map(col => String(obj[col]));
+        //righe.push(valori.join(""));
+        let output = "";
+        righe.push(`memory : ${obj.memory}`);
+        righe.push( `OS : ${obj.OS}`);
+        righe.push(`quantità : ${obj.quantità}`);
+        righe.push(`name : ${obj.name}`);
+        righe.push("-".repeat(50));
     });
 
     // Creazione del contenuto stream con posizionamento corretto
