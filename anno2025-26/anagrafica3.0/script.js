219be08893c4317deabb44bb254f28a348bdcb4f
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-12-09 18:10:44 +0100

    anagrafica3.0

--- anno2025-26/anagrafica3.0/script.js
+++ anno2025-26/anagrafica3.0/script.js
@@ -3,15 +3,15 @@ sessionStorage.getItem(1) !== null ? array = JSON.parse(sessionStorage.getItem(1
 sessionStorage.setItem(1, JSON.stringify(array, null, 2));
 function samsung() {
     sessionStorage.setItem(0, "samsung");
-    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/Rubi_TechStore/pagina2/index.html";
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
 function apple() {
     sessionStorage.setItem(0, "apple");
-    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/Rubi_TechStore/pagina2/index.html";
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
 function huawei() {
     sessionStorage.setItem(0, "huawei");
-    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/Rubi_TechStore/pagina2/index.html";
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
 }
 
 const subMenus = document.querySelectorAll(".sub-menu"),
