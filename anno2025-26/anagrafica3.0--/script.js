1926e9a644cbeeab3ca35676c2a5f45240b2d061
Matteo Rubino <145324137+RubinoMatteo@users.noreply.github.com>
2025-12-09 18:05:56 +0100

    anagrafica3.0

---
+++ anno2025-26/anagrafica3.0--/script.js
@@ -0,0 +1,52 @@
+let array=[];
+sessionStorage.getItem(1) !== null? array=JSON.parse(sessionStorage.getItem(1)) : array[0]={name:"nome",memory:"memoria",OS:"Sistema Operativo"};
+sessionStorage.setItem(1, JSON.stringify(array, null, 2));
+function samsung(){
+    sessionStorage.setItem(0, "samsung");
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
+}
+function apple(){
+    sessionStorage.setItem(0, "apple");
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
+}
+function huawei(){
+    sessionStorage.setItem(0, "huawei");
+    location.href = "https://rubinomatteo.github.io/lavoriScuola/anno2025-26/anagrafica3.0/pagina2/index.html";
+}
+
+function vediCarrello(){
+    let carrello = document.getElementById("carrello");
+    const output = document.createElement("div");
+    output.className="card";
+    const rawData = sessionStorage.getItem("1");
+        if (rawData) {
+        let dataArray;
+        try {
+            dataArray = JSON.parse(rawData); 
+        } catch (e) {
+            output.innerHTML = "<p style='color: red;'>Errore nel leggere il JSON</p>";
+            throw e;
+        }
+        if (!Array.isArray(dataArray)) {
+            output.innerHTML = "<p style='color: red;'>Il dato non è un array</p>";
+            return;
+        }
+        dataArray.slice(1).forEach((item, index) => {
+            let ul = document.createElement("ul");
+            if (index === 0) {
+                ul.innerHTML += `<h4 style="margin:0 0 10px; color:#555;">Intestazione</h4>`;
+            } else {
+                ul.innerHTML += `<h4 style="margin:0 0 10px; color:#555;">Elemento ${index}</h4>`;
+            }
+            Object.entries(item).forEach(([key, value]) => {
+                let li = document.createElement("li");
+                li.innerHTML = `<strong style="color:#667eea;">${key}:</strong> ${value}`;
+                ul.appendChild(li);
+            });
+            output.appendChild(ul);
+        });
+    } else {
+        output.innerHTML = `<p style="color: red;">Nessun dato ricevuto</p>`;
+    }
+    carrello.appendChild(output);
+}
--- anno2025-26/anagrafica3.0/pagina2/script.js
+++ anno2025-26/anagrafica3.0/pagina2/script.js
@@ -11,13 +11,81 @@ switch(sessionStorage.getItem(0)) {
   default:
     errore();
 }
-function scrivi(nome,memory,os,image){
+
+const subMenus = document.querySelectorAll(".sub-menu"),
+    btns = document.querySelectorAll("button"),
+    sidebar = document.querySelector(".sidebar"),
+    headerImg = document.querySelector(".sidebar header img");
+
+const reset = () => {
+    btns.forEach(btn => btn.classList.remove("active"));
+    subMenus.forEach(menu => (menu.style.height = 0));
+};
+
+const openSubmenu = element => {
+    // Non permettere di aprire submenu se la sidebar è chiusa
+    if (sidebar.classList.contains("collapsed")) return;
+    
+    reset();
+    element.classList.add("active");
+    const sibling = element.nextElementSibling;
+    const ul = sibling.querySelector("ul");
+    if (sibling.clientHeight == 0) {
+        sibling.style.height = `${ul.clientHeight}px`;
+    } else {
+        sibling.style.height = 0;
+        element.classList.remove("active");
+    }
+};
+// Toggle sidebar quando clicchi sull'immagine
+headerImg.addEventListener("click", () => {
+    sidebar.classList.toggle("collapsed");
+    // Mostra/nascondi carrello in base allo stato della sidebar
+    if (!sidebar.classList.contains("collapsed")) {
+        vediCarrello();
+    } else {
+        // Svuota il carrello quando la sidebar si chiude
+        document.getElementById("carrello").innerHTML = "";
+    }
+    // Chiudi tutti i submenu quando collassi la sidebar
+    if (sidebar.classList.contains("collapsed")) {
+        reset();
+    }
+});
+function vediCarrello() {
+    let carrello = document.getElementById("carrello");
+    carrello.innerHTML = "";
+    const output = document.createElement("div");
+    output.className = "card";
+    let dataArray = contaElementi();
+    if (!Array.isArray(dataArray)) {
+        output.innerHTML = "<p style='color: red;'>Il dato non è un array</p>";
+        return;
+    }
+    dataArray.forEach((item, index) => {
+        let ul = document.createElement("ul");
+        ul.innerHTML += `<h4 style="margin:0 0 10px; color:#555;">Elemento ${index}</h4>`;
+        Object.entries(item).forEach(([key, value]) => {
+            let li = document.createElement("li");
+            li.innerHTML = `<strong style="color:#667eea;">${key}:</strong> ${value}`;
+            ul.appendChild(li);
+        });
+        output.appendChild(ul);
+    });
+    carrello.appendChild(output);
+    carrello.innerHTML += `<br><div class="card">
+                                <a class="button" onclick="scaricaPDF(event)" href="" id="linkScaricaxml">&#x1f6d2; download PDF &#10515;</a>
+                                </div>`;
+}
+
+function scrivi(nome,memory,os,image,prezzo){
     return`<div class="card">
                             <h3>${nome}</h3>
                             <p><b>Memoria</b>:${memory}</p>
                             <p><b>S.O.</b>:${os}</p>
                             <img id="img" src="${image}" alt="samsung"> 
-                            <a target="_blank" class="button" onclick="acquista('${nome}','${memory}','${os}')" > &#128722; </a>
+                            <p><b>Prezzo</b>: EUR ${prezzo}</p>
+                            <a target="_blank" class="button" onclick="acquista('${nome}','${memory}','${os}','${prezzo}')" style="cursor: pointer;" > &#128722; </a>
                         </div>`;
 }
 function samsung(){
@@ -29,7 +97,7 @@ xmlhttp.open("GET", "samsung.json", true);
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             for (x in myObj.samsung) 
-                stampa += scrivi(myObj.samsung[x].name,myObj.samsung[x].memory,myObj.samsung[x].os,myObj.samsung[x].image);
+                stampa += scrivi(myObj.samsung[x].name,myObj.samsung[x].memory,myObj.samsung[x].os,myObj.samsung[x].image,myObj.samsung[x].prezzo);
             document.getElementById("demo").innerHTML = `${stampa}`;
             stampa="";
         }
@@ -49,7 +117,8 @@ function apple(){
                 var memory = iPhone[x].getElementsByTagName("memory")[0].childNodes[0].nodeValue;
                 var os = iPhone[x].getElementsByTagName("os")[0].childNodes[0].nodeValue;
                 var image = iPhone[x].getElementsByTagName("image")[0].childNodes[0].nodeValue;
-                stampa+=scrivi(name,memory,os,image);
+                var prezzo = iPhone[x].getElementsByTagName("prezzo")[0].childNodes[0].nodeValue;
+                stampa+=scrivi(name,memory,os,image,prezzo);
             }            
             document.getElementById("demo").innerHTML = `${stampa}`;
             stampa = "";
@@ -74,7 +143,7 @@ function huawei(){
             var xmlDoc = this.responseText;
             var huawei = dividi(xmlDoc);
             for (let i = 1; i < huawei.length; i++) {
-                stampa+=scrivi(huawei[i][0],huawei[i][1],huawei[i][2],huawei[i][3]);
+                stampa+=scrivi(huawei[i][0],huawei[i][1],huawei[i][2],huawei[i][3],huawei[i][4]);
             }
             document.getElementById("demo").innerHTML = `${stampa}`;
             stampa = "";
@@ -86,149 +155,21 @@ function errore(){
 };
 var carrello=[];
 let quantità=0;
-function acquista(n,m,S){
+function acquista(n,m,S,p){
     carrello=JSON.parse(sessionStorage.getItem(1));
     quantità=JSON.parse(sessionStorage.getItem(1)).length;
-    carrello[quantità]= {name:n,memory:m,OS:S};
+    carrello[quantità]= {name:n,memory:m,OS:S,prezzo:p};
     quantità++;
     sessionStorage.setItem(1, JSON.stringify(carrello, null, 2));
     console.log(carrello);
 };
-/*const linkScarica = document.getElementById('linkScaricaJson');
-linkScarica.addEventListener('click', function(event) {
-    const stringaJson = sessionStorage.getItem(1); 
-    const blob = new Blob([stringaJson], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    linkScarica.href = url;
-    setTimeout(() => {
-        URL.revokeObjectURL(url);
-    }, 1000);
-    sessionStorage.clear();
-});*//*
-function scarica(event){
-    event.preventDefault();
-    const stringaJson = sessionStorage.getItem(1); 
-    if (!stringaJson) {
-        alert("Il carrello è vuoto!");
-        return;
-    }
-    const blob = new Blob([stringaJson], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    linkScarica.href = url;
-    linkScarica.download = "carrello_acquisti.json";
-    linkScarica.click();
-    setTimeout(() => {
-        URL.revokeObjectURL(url);
-    }, 1000);
-}*/
-function scarica(event){
-    event.preventDefault();
-    const stringaJson = sessionStorage.getItem("1"); 
-    if (!stringaJson) {
-        alert("Il carrello è vuoto!");
-        return;
-    }
-    const blob = new Blob([stringaJson], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = "carrello_acquisti.json";
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-    setTimeout(() => {
-        URL.revokeObjectURL(url);
-    }, 1000);
-}
-function scaricacsv(event) {
-    event.preventDefault();
-    const stringaJson = sessionStorage.getItem("1");
-    if (!stringaJson) {
-        alert("Il carrello è vuoto!");
-        return;
-    }
-    let dati;
-    try {
-        dati = JSON.parse(stringaJson);
-    } catch (e) {
-        alert("Errore: dati non validi!");
-        return;
-    }
-    if (!Array.isArray(dati)) {
-        dati = [dati];
-    }
-    const colonne = Object.keys(dati[0]);
-    const righe = [];
-    righe.push(colonne.join(";"));
 
-    dati.forEach(obj => {
-        const valori = colonne.map(col => String(obj[col]).replace(/;/g, ",")); // evita rottura colonne
-        righe.push(valori.join(";"));
-    });
-
-    const csvContent = righe.join("\n");
-
-    const blob = new Blob([csvContent], { type: 'text/csv' });
-    const url = URL.createObjectURL(blob);
-    
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = "carrello_acquisti.csv";
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-
-    setTimeout(() => URL.revokeObjectURL(url), 1000);
-}
-function scaricaxml(event) {
-    event.preventDefault();
-    const stringaJson = sessionStorage.getItem("1");
-    if (!stringaJson) {
-        alert("Il carrello è vuoto!");
-        return;
-    }
-    let dati;
-    try {
-        dati = JSON.parse(stringaJson);
-    } catch (e) {
-        alert("Errore: dati non validi!");
-        return;
-    }
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<carrello>\n`;
-    dati.forEach(item => {
-        xml += "  <item>\n";
-        for (const chiave in item) {
-            const valore = String(item[chiave])
-                .replace(/&/g, "&amp;")
-                .replace(/</g, "&lt;")
-                .replace(/>/g, "&gt;");
-            xml += `    <${chiave}>${valore}</${chiave}>\n`;
-        }
-        xml += "  </item>\n";
-    });
-    xml += "</carrello>";
-    const blob = new Blob([xml], { type: "application/xml" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "carrello_acquisti.xml";
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-    setTimeout(() => URL.revokeObjectURL(url), 1000);
-}
 function scaricaPDF(event) {
     event.preventDefault();
 
-    const stringaJson = sessionStorage.getItem("1");
-    if (!stringaJson) {
-        alert("Il carrello è vuoto!");
-        return;
-    }
-
     let dati;
     try {
-        dati = JSON.parse(stringaJson);
+        dati = contaElementi();
     } catch (e) {
         alert("Errore: dati non validi!");
         return;
@@ -238,37 +179,100 @@ function scaricaPDF(event) {
         dati = [dati];
     }
 
-    const colonne = Object.keys(dati[0]);
-
-    // Costruzione del contenuto testuale per il PDF
-    let righe = [];
-    righe.push("CARRELLO ACQUISTI");
-    righe.push("");
-    righe.push(colonne.join(" | "));
-    righe.push("-".repeat(50));
-
+    // Informazioni dell'emittente
+    const emittente = {
+        nome: "Rubi TechStore S.r.l.",
+        indirizzo: "Via Raffaello Sanzio, 2",
+        citta: "Rho, 20017",
+        piva: "P.IVA: 12345678901",
+        telefono: "Tel: +39 02 1234567"
+    };
+    
+    let divisore = "=".repeat(35);
+    // Calcolo totale
+    let totale = 0;
     dati.forEach(obj => {
-        const valori = colonne.map(col => String(obj[col]));
-        righe.push(valori.join(" | "));
+        const prezzo = obj.prezzo || 999.99; // Prezzo di default se non specificato
+        const quantita = obj.quantità || 1;
+        totale += prezzo * quantita;
     });
 
     // Creazione del contenuto stream con posizionamento corretto
     let yPos = 800;
-    let textCommands = "BT\n/F1 12 Tf\n";
+    let textCommands = "BT\n/F1 10 Tf\n";
+
+    // Helper per aggiungere testo centrato
+    function addCenteredText(text, y) {
+        const testoEscaped = text.replace(/\\/g, "\\\\").replace(/\(/g, "\\(").replace(/\)/g, "\\)");
+        textCommands += `0 ${y - yPos} Td\n(${testoEscaped}) Tj\n`;
+        yPos = y;
+    }
+
+    // Helper per aggiungere testo normale
+    function addText(text, xOffset = 0) {
+        const testoEscaped = text.replace(/\\/g, "\\\\").replace(/\(/g, "\\(").replace(/\)/g, "\\)");
+        textCommands += `${xOffset} -15 Td\n(${testoEscaped}) Tj\n`;
+    }
+
+    // INTESTAZIONE
+    textCommands += `200 ${yPos} Td\n`; // Posizionamento iniziale centrato
     
-    righe.forEach((riga, index) => {
-        // Escape delle parentesi nel testo
-        const testoEscaped = riga.replace(/\\/g, "\\\\").replace(/\(/g, "\\(").replace(/\)/g, "\\)");
+    // Logo (testo nome negozio) - in un PDF reale potresti inserire un'immagine
+    textCommands += `/F1 14 Tf\n`;
+    addText("*** RUBI TECHSTORE ***", 0);
+
+    //informazionni emittente 
+    textCommands += `/F1 9 Tf\n`;
+    addText(emittente.nome, 0);
+    addText(emittente.indirizzo, 0);
+    addText(emittente.citta, 0);
+    addText(emittente.piva, 0);
+    addText(emittente.telefono, 0);
+
+    addText("", 0); // Riga vuota
+    addText(divisore, 0);
+    
+    // Data e ora
+    const now = new Date();
+    const dataOra = `Data: ${now.toLocaleDateString('it-IT')} ${now.toLocaleTimeString('it-IT')}`;
+    addText(dataOra, 0);
+    addText(divisore, 0);
+    addText("", 0);
+
+    // PRODOTTI
+    addText("DESCRIZIONE", 0);
+    addText("", 0);
+
+    dati.forEach(obj => {
+        const nome = obj.name || "Prodotto";
+        const quantita = obj.quantità || 1;
+        const prezzo = obj.prezzo || 999.99;
+        const totaleRiga = (prezzo * quantita).toFixed(2);
         
-        if (index === 0) {
-            // Prima riga - posizionamento assoluto
-            textCommands += `50 ${yPos} Td\n(${testoEscaped}) Tj\n`;
-        } else {
-            // Righe successive - spostamento relativo verso il basso
-            textCommands += `0 -15 Td\n(${testoEscaped}) Tj\n`;
-        }
+        // Riga prodotto: quantità x nome
+        addText(`${quantita} x ${nome}`, 0);
+        
+        // Prezzo allineato a destra (simulato con spazi)
+        const prezzoStr = `EUR ${totaleRiga}`;
+        const spaces = " ".repeat(Math.max(0, 35 - prezzoStr.length));
+        addText(`${spaces}${prezzoStr}`, 0);
+        addText("", 0);
     });
+
+    // TOTALE
+    addText(divisore, 0);
+    const totaleStr = `EUR ${totale.toFixed(2)}`;
+    const spacesTotale = " ".repeat(Math.max(0, 25 - totaleStr.length));
+    textCommands += `/F1 12 Tf\n`;
+    addText(`TOTALE:${spacesTotale}${totaleStr}`, 0);
+    textCommands += `/F1 9 Tf\n`;
+    addText(divisore, 0);
     
+    addText("", 0);
+    addText("Grazie per il suo acquisto!", 0);
+    addText("", 0);
+    addText("IVA inclusa 22%", 0);
+
     textCommands += "ET";
 
     // Array per memorizzare gli oggetti
@@ -293,7 +297,7 @@ function scaricaPDF(event) {
     addObject(`4 0 obj\n<< /Length ${textCommands.length} >>\nstream\n${textCommands}\nendstream\nendobj\n`);
 
     // Oggetto 5 – Font
-    addObject("5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n");
+    addObject("5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Courier >>\nendobj\n");
 
     // Costruzione del PDF finale
     let pdf = "%PDF-1.4\n";
@@ -323,16 +327,50 @@ function scaricaPDF(event) {
     pdf += `${xrefPos}\n`;
     pdf += "%%EOF";
 
-    // Download del file
+        // Download del file
     const blob = new Blob([pdf], { type: "application/pdf" });
     const url = URL.createObjectURL(blob);
 
     const a = document.createElement("a");
     a.href = url;
-    a.download = "carrello_acquisti.pdf";
+    a.download = "scontrino_" + Date.now() + ".pdf";
     document.body.appendChild(a);
     a.click();
     a.remove();
 
     setTimeout(() => URL.revokeObjectURL(url), 1000);
+}
+
+function contaElementi() {
+    let scontrino = [];
+    let index = [];
+    let conta = 0;
+    let data = JSON.parse(sessionStorage.getItem("1"));
+    if (data.length > 1) {
+        let dati = data.slice(1);
+        for (let i = 0; i < dati.length; i++) {
+            for (let j = 0; j < dati.length; j++) {
+                if (dati[i].name == dati[j].name) {
+                    conta++;
+                    if (conta > 1)
+                        index.push(j);
+                }
+            }
+            let controllo = false ;
+            if(conta>1)
+                for (let j = 0; j < scontrino.length; j++) 
+                    if (dati[i].name == scontrino[j].name) 
+                        controllo=true;
+            if(!controllo){
+            let obj = { name: dati[i].name, memory: dati[i].memory, OS: dati[i].OS, prezzo: dati[i].prezzo, quantità: conta };
+            scontrino.push(obj);
+            }
+            conta = 0;
+        }
+        console.log(scontrino);
+        return scontrino;
+    } else {
+        alert("il carrello é vuoto");
+        return;
+    }
 }
